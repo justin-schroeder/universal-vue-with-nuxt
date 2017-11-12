13aa823a8ba015545018321091b686d15ad6cc4b
Justin Schroeder <justinschroeder@Justins-MacBook-Pro.local>
2017-11-12 15:20:03 -0500

    Adds the initial Vue SSR example-02

---
+++ 02-vue/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs')
+const renderer = require('vue-server-renderer').createRenderer()
+const template = fs.readFileSync('./index.html', 'utf-8')
+const app = require('./universal.js')
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/universal.js":
+      res.end(fs.readFileSync('./universal.js', 'utf-8'))
+      break;
+    default:
+      renderer.renderToString(app, (err, html) => {
+        if (err) throw err
+        res.end(template.replace('${app}', html))
+      })
+  }
+})
+
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(3000)
