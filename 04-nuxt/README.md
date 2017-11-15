1ac6548150afe62483a48b468f3dd10c5fb441d7
Justin Schroeder <justinschroeder@Justins-MacBook-Pro.local>
2017-11-15 13:21:09 -0500

    Adds 04-Nuxt example

---
+++ 04-nuxt/README.md
@@ -0,0 +1,13 @@
+# 04-nuxt
+---------
+
+In this example we recreate 01-vanilla using Nuxt.
+
+To run it:
+
+```sh
+npm install
+npm run dev
+```
+
+Then browse [http://localhost:3000](http://localhost:3000).
---
+++ 04-nuxt/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="time">Current time is: {{ hours }}:{{ minutes }}:{{ seconds }}.{{ milliseconds }}</div>
+    <small><span v-html="copy"></span> {{ year }} Braid LLC. Absolutely no rights reserved.</small>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: new Date(),
+      copy: '&copy;'
+    }
+  },
+  head () {
+    return {
+      title: '04-Example: Nuxt'
+    }
+  },
+  computed: {
+    year () {
+      return this.date.getFullYear()
+    },
+    hours () {
+      return this.date.getHours()
+    },
+    minutes () {
+      return this.date.getMinutes()
+    },
+    seconds () {
+      return this.date.getSeconds()
+    },
+    milliseconds () {
+      return this.date.getMilliseconds()
+    }
+  },
+  mounted () {
+    setInterval(() => { this.date = new Date() }, 100)
+  }
+}
+</script>
